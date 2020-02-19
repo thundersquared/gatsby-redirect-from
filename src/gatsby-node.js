8e5324bcb56effc3337d63b9fc0bf3e11122f610
Anatoli Nicolae <desk@anatolinicolae.com>
2020-02-19 17:44:47 +0100

    Implement GraphQL query fix
    Signed-off-by: Anatoli Nicolae <desk@anatolinicolae.com>

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,67 +1,63 @@
 import chalk from 'chalk'
 
-export function createPages({ graphql, actions }, pluginOptions) {
-  const { createRedirect } = actions
+export function createPages({graphql, actions}, pluginOptions) {
+    const {createRedirect} = actions
 
-  const markdownQuery = pluginOptions.query || 'allMarkdownRemark'
+    const markdownQuery = pluginOptions.query || 'allMarkdownRemark'
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
+    return new Promise((resolve, reject) => {
+        resolve(
+            graphql(`
           {
             q: ${markdownQuery}(
               filter: { frontmatter: { redirect_from: { ne: null } } }
             ) {
               edges {
                 node {
-                  fields {
-                    slug
-                  }
                   frontmatter {
                     redirect_from
+                    slug
                   }
                 }
               }
             }
           }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors) // eslint-disable-line no-console
-          reject(result.errors)
-        }
+        `).then(result => {
+                if (result.errors) {
+                    console.log(result.errors) // eslint-disable-line no-console
+                    reject(result.errors)
+                }
 
-        const allPosts = result.data.q.edges
+                const allPosts = result.data.q.edges
 
-        const redirects = []
+                const redirects = []
 
-        // For all posts with redirect_from frontmatter,
-        // extract all values and push to redirects array
-        allPosts.forEach(post => {
-          redirects.push({
-            from: post.node.frontmatter.redirect_from,
-            to: post.node.fields.slug
-          })
-        })
+                // For all posts with redirect_from frontmatter,
+                // extract all values and push to redirects array
+                allPosts.forEach(post => {
+                    redirects.push({
+                        from: post.node.frontmatter.redirect_from,
+                        to: post.node.frontmatter.slug,
+                    })
+                })
 
-        // Create redirects from the just constructed array
-        redirects.forEach(({ from, to }) => {
-          // iterate through all `from` array items
-          from.forEach(from => {
-            createRedirect({
-              fromPath: from,
-              toPath: to,
-              isPermanent: true,
-              redirectInBrowser: true
-            })
-          })
-        })
+                // Create redirects from the just constructed array
+                redirects.forEach(({from, to}) => {
+                    // iterate through all `from` array items
+                    from.forEach(from => {
+                        createRedirect({
+                            fromPath: from,
+                            toPath: to,
+                            isPermanent: true,
+                            redirectInBrowser: true,
+                        })
+                    })
+                })
 
-        resolve(
-          console.log(`${chalk.green('success')} create redirects`) // eslint-disable-line no-console
+                resolve(
+                    console.log(`${chalk.green('success')} create redirects`), // eslint-disable-line no-console
+                )
+            }),
         )
-      })
-    )
-  })
+    })
 }
